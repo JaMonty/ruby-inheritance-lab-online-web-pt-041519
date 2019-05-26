db73408667c18399c61347817f0fa57db31ae3fb
Jason  Montes <jasone.montes@gmail.com>
2019-05-26 15:57:50 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,7 +2,7 @@ class User
   
   attr_accessor :first_name, :last_name
   
-  def initialize(first_name , last_name)
+  def initialize(first_name = fn, last_name = ln)
     @first_name = first_name
     @last_name = last_name
   end
